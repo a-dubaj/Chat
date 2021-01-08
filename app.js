03b4307db264f68526506dc35a03d090949ccd63
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-01-08 14:54:23 +0100

    Form for user name and chatroom

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
+
+//Render Index page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on port ${port}`)
+})
